d95f985af42fd39bbc4d81e82daa14312e02b479
Rafael Kimura <rafaelkimura57@gmail.com>
2024-12-24 20:14:40 -0300

    Video 03: Commit inicial do projeto

---
+++ kimura-projects/src/main/java/academy/devdojo/kimura/ProjetoDiretoDasTrincheirasApplication.java
@@ -0,0 +1,13 @@
+package academy.devdojo.kimura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDiretoDasTrincheirasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDiretoDasTrincheirasApplication.class, args);
+	}
+
+}
---
+++ kimura-projects/src/test/java/academy/devdojo/kimura/ProjetoDiretoDasTrincheirasApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.devdojo.kimura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDiretoDasTrincheirasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
